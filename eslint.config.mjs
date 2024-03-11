71b16c1e2ddfcde3bcd18790c87caa21f94fda30
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2024-03-11 12:38:58 +0800

    chore: update

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  stylistic: {
+    overrides: {
+      'style/jsx-one-expression-per-line': 'off',
+    },
+  },
+  typescript: {
+    overrides: {
+      'ts/ban-ts-comment': 'off',
+    },
+  },
+  vue: {
+    overrides: {
+      'vue/html-self-closing': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+    },
+  },
+})
