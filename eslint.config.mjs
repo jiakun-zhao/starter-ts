065ed09709d51cf9aed6ec56e496f3de9b0c5019
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2024-09-21 20:16:25 +0800

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,19 +5,11 @@ export default defineConfig({
     overrides: {
       'no-console': 'warn',
       'style/brace-style': ['warn', '1tbs'],
-      // typescript
       'ts/ban-ts-comment': 'off',
-      // unused-imports
+      'ts/no-unused-expressions': 'off',
       'unused-imports/no-unused-imports': 'warn',
-      // jsx
-      'style/jsx-quotes': ['off', 'prefer-single'],
+      'style/jsx-quotes': ['warn', 'prefer-single'],
       'style/jsx-one-expression-per-line': 'off',
     },
   },
-  // vue: {
-  //   overrides: {
-  //     'vue/html-self-closing': 'off',
-  //     'vue/singleline-html-element-content-newline': 'off',
-  //   },
-  // },
 })
