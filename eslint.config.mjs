f27a747fd96bdd4260c66205e88240e3ac1792ba
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2025-01-15 10:47:35 +0800

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
+/* eslint perfectionist/sort-objects: "error" */
+
 import defineConfig from '@antfu/eslint-config'
 
 export default defineConfig({
-  stylistic: {
-    overrides: {
-      'no-console': 'warn',
-      'style/brace-style': ['warn', '1tbs'],
-      'ts/ban-ts-comment': 'off',
-      'ts/no-unused-expressions': 'off',
-      'unused-imports/no-unused-imports': 'warn',
-      'style/jsx-quotes': ['warn', 'prefer-single'],
-      'style/jsx-one-expression-per-line': 'off',
-    },
+  rules: {
+    'no-console': 'warn',
+    'style/brace-style': ['warn', '1tbs'],
+    'style/jsx-one-expression-per-line': 'off',
+    'style/jsx-quotes': ['warn', 'prefer-single'],
+    'ts/ban-ts-comment': 'off',
+    'ts/no-unused-expressions': 'off',
+    'unused-imports/no-unused-imports': 'warn',
   },
 })
