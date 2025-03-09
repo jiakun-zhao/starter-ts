d84b46d175f349f63280f02952c9418292298c2c
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2025-03-09 16:36:54 +0800

    chore: remove cjs and update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,10 @@ import defineConfig from '@antfu/eslint-config'
 
 export default defineConfig({
   rules: {
+    // 'style/jsx-one-expression-per-line': 'off',
+    // 'style/jsx-quotes': ['warn', 'prefer-single'],
     'no-console': 'warn',
     'style/brace-style': ['warn', '1tbs'],
-    'style/jsx-one-expression-per-line': 'off',
-    'style/jsx-quotes': ['warn', 'prefer-single'],
     'ts/ban-ts-comment': 'off',
     'ts/no-unused-expressions': 'off',
     'unused-imports/no-unused-imports': 'warn',
--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,6 @@ import { describe, expect, it } from 'vitest'
 
 describe('test', () => {
   it('should work', () => {
-    expect(true).toBe(true)
+    expect(true).toBeTruthy()
   })
 })
